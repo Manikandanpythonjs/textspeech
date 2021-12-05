d29eab59e0a91c3311c27c15a88c9bcd4d9abb9b
manikandanpythonjs <manipythonjs@gmail.com>
2021-12-05 04:28:39 -0800

    commit

--- Text_To_Speech_Dj/settings.py
+++ Text_To_Speech_Dj/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = BASE_DIR / "statics"
 
 STATICFILES_DIRS =[
-    # BASE_DIR / "static",
-    os.path.join(BASE_DIR,'static'),
+    BASE_DIR / "statics",
+    # os.path.join(BASE_DIR,'static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
